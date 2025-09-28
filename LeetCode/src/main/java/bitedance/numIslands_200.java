1ce360b10efc807519424cc31ce23eb99d3b9364
qsl746 <64734954+qsl746@users.noreply.github.com>
2025-09-29 03:16:17 +0800

    岛屿数量-200-dfs深度优先

--- LeetCode/src/main/java/bitedance/numIslands_200.java
+++ LeetCode/src/main/java/bitedance/numIslands_200.java
@@ -3,24 +3,41 @@ package bitedance;
 public class numIslands_200 {
     class Solution {
         public int numIslands(char[][] grid) {
-            int count = 0;
-            for(int i = 0; i < grid.length; i++) {
-                for(int j = 0; j < grid[0].length; j++) {
-                    if(grid[i][j] == '1'){
-                        dfs(grid, i, j);
+            int count=0;
+
+            for (int i = 0; i < grid.length; i++) {
+                for(int j=0;j<grid[0].length;j++){
+                    if (grid[i][j]=='1'){
+                        dfs(grid,i,j);
                         count++;
                     }
                 }
             }
+
+
             return count;
         }
-        private void dfs(char[][] grid, int i, int j){
-            if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') return;
-            grid[i][j] = '0';
-            dfs(grid, i + 1, j);
-            dfs(grid, i, j + 1);
-            dfs(grid, i - 1, j);
-            dfs(grid, i, j - 1);
+
+        void dfs(char [][] grid,int i,int j){
+            if(!isInGrid(grid,i,j)){
+                //超出表格就退出
+                return;
+            }
+            if (grid[i][j]!='1'){
+                //不是未遍历的岛屿面积就退出
+                return;
+            }
+
+            grid[i][j]='2';
+
+            dfs(grid,i-1,j);
+            dfs(grid,i+1,j);
+            dfs(grid,i,j-1);
+            dfs(grid,i,j+1);
+        }
+
+        public boolean isInGrid(char[][] grid,int i,int j){
+            return 0<=i&&i< grid.length&&0<=j&&j<=grid[0].length;
         }
     }
 
