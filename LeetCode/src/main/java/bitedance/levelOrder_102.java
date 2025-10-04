edf664dde2b9707630cc1084fdb30806626f8934
qsl746 <64734954+qsl746@users.noreply.github.com>
2025-10-04 17:46:40 +0800

    层序遍历-102

---
+++ LeetCode/src/main/java/bitedance/levelOrder_102.java
@@ -0,0 +1,43 @@
+package bitedance;
+
+import common.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class levelOrder_102 {
+    class Solution {
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            List<List<Integer>> res=new ArrayList<>();
+            if (root==null)return res;
+
+            //用列表，两个计数值，一个统计当前层有几个，一个统计下一层
+            Queue<TreeNode> queue=new ArrayDeque<>();
+            queue.add(root);
+            int currentLevel=1;
+            while (!queue.isEmpty()){
+                List<Integer> temp=new ArrayList<>();
+                int nextLevel=0;
+                while (currentLevel!=0){
+                    TreeNode pollNode = queue.poll();
+                    temp.add(pollNode.val);
+                    if (pollNode.left!=null){
+                        queue.add(pollNode.left);
+                        nextLevel++;
+                    }
+                    if (pollNode.right!=null){
+                        queue.add(pollNode.right);
+                        nextLevel++;
+                    }
+                    currentLevel--;
+                }
+                currentLevel=nextLevel;
+                res.add(temp);
+            }
+            return res;
+        }
+
+    }
+}
